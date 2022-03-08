8fe803fdb0160cfa5350282a01bc68b3e63ebcdb
Michael <Michael@DESKTOP-RHUN95E.mshome.net>
2022-03-08 17:12:48 -0300

    coomit

---
+++ src/controller/KillController.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class KillController {
+
+	public void listaProcessos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mataPid(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mataPid(String i) {
+		// TODO Auto-generated method stub
+		
+	}
+}
